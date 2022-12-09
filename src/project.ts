e618e021e4b2953a88e9100d0d704b59f5aefce2
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-12-09 18:31:12 +0900

    Project apiBaseUri is changed (#6)

--- src/project.ts
+++ src/project.ts
@@ -5,12 +5,18 @@ export class Project extends MongoDbAtlasBase {
   constructor(publicKey: string, privateKey: string, projectId: string){
     super(publicKey, privateKey);
     this.projectId = projectId;
+    this.apiBaseUri = `${this.apiBaseUri}/groups/${projectId}`;
   }
-  public async get(){
-    return await super.get(`${this.apiBaseUri}/groups/${this.projectId}`);
+  public async get(url?: string){
+    return await super.get(url ?? this.apiBaseUri);
   }
   public async getFreeClusters(){
-    return await super.get(`${this.apiBaseUri}/groups/${this.projectId}/clusters`)
+    return await super.get(`${this.apiBaseUri}/clusters`)
       .then(({ results }) => results.filter((cluster: any) => cluster?.providerSettings?.instanceSizeName === "M0"));
+  }
+  public async getUsers(){
+    return await this.getAll({
+      url: `${this.apiBaseUri}/databaseUsers`
+    });
   }
 }
