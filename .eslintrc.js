41a95336360e82ab179ba1b3bb1f63214200b466
tom <toms@live.jp>
2022-12-10 14:30:23 +0900

    eslint is added

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard-with-typescript',
+    "prettier",
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: "./tsconfig.base.json",
+  },
+  rules: {
+  }
+}
