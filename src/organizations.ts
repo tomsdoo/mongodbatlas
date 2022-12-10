1039da723a6f2e1e547b51123790d9c0f7031a13
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-12-10 10:11:31 +0900

    Organizations (#13)

---
+++ src/organizations.ts
@@ -0,0 +1,8 @@
+import { MongoDbAtlasBase } from "./base";
+
+export class Organizations extends MongoDbAtlasBase {
+  constructor(publicKey: string, privateKey: string){
+    super(publicKey, privateKey);
+    this.apiBaseUri = `${this.apiBaseUri}/orgs`;
+  }
+}
